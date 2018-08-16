3297909dc890d0a030230561b6f461f9a884e373
Matt Andrews <mattandrews@massey.com.au>
2018-08-17 09:44:28 +1000

    Added sales.get_quotes Added jsonschema/sales/get-quotes

--- modules/sales/functions/get-quotes.sql
+++ modules/sales/functions/get-quotes.sql
@@ -1,4 +1,7 @@
-CREATE OR REPLACE FUNCTION sales.list_quotes (json, OUT result json) AS
+/*
+Get quotes for a given order
+*/
+CREATE OR REPLACE FUNCTION sales.get_quotes (json, OUT result json) AS
 $$
 BEGIN
   SELECT json_strip_nulls(json_agg(r)) INTO result
@@ -20,13 +23,7 @@ BEGIN
       ON buyer.party_id = o.buyer_id
     LEFT JOIN party_v contact -- Left join as a document may not have a contact
       ON contact.party_id = q.contact_id
-    WHERE to_tsvector(
-      concat_ws(' ',
-        q.order_id,
-        buyer.name,
-        contact.name
-      )
-    ) @@ plainto_tsquery($1->>'search')
+    WHERE o.order_id = ($1->>'quoteId')
   ) r;
 END
 $$
